d8fe55b97f0aec6bf89d77a7c88325cd66c2a97b
Akahad1 <aksahad234567@gmail.com>
2024-12-05 12:22:36 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,15 @@
+const persons = [
+  { name: "Rakib", age: 20, gender: "male" },
+  { name: "Ria", age: 20, gender: "Female" },
+  { name: "Nokive", age: 20, gender: "male" },
+  { name: "Shkive", age: 20, gender: "male" },
+  { name: "Piya", age: 20, gender: "Female" },
+];
+
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const result = filterMale(persons);
+console.log(result);
---
+++ problem2.js
@@ -0,0 +1,15 @@
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "Gatsby", author: "George Orwell", year: 1949 },
+  { title: "Moby Dick", author: "Herman Melville", year: 1851 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+];
+
+function booksTitel(bookArry) {
+  const newArry = bookArry.map((book) => book.title);
+  return newArry;
+}
+
+const result = booksTitel(books);
+console.log(result);
