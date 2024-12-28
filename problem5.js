4c3e0602ef866607c07c39d80bf18d90ff874e29
Akahad1 <aksahad234567@gmail.com>
2024-12-28 22:47:22 +0600

    problem678

--- problem5.js
+++ problem5.js
@@ -17,3 +17,42 @@ function modifyAgeByName(peopleArray, personName, newAge) {
 }
 const updatedPeople = modifyAgeByName(people, "piya", 32);
 console.log(updatedPeople);
+const uniqueNumbers = (arr) => {
+  const freq = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freq)
+    .filter((key) => freq[key] === 1)
+    .map(Number);
+};
+
+console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
+
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
+
+const frequency = (arr) => {
+  return arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+console.log(frequency([1, 2, 2, 3, 3, 3])); // {1: 1, 2: 2, 3: 3}
+
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
