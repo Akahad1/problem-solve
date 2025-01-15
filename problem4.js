759b38e3b527131c1bdec51d51a8dc930d4ee73b
Akahad1 <aksahad234567@gmail.com>
2025-01-15 19:56:39 +0600

    add problem9

--- problem4.js
+++ problem4.js
@@ -31,3 +31,15 @@ function findIntersection(arr1, arr2) {
   return arr1.filter((num) => arr2.includes(num));
 }
 console.log(findIntersection([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+console.log(isBalanced("(())"));
