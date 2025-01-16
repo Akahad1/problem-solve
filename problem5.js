9f8e607111c874b7b89ef0585185de4753ddefc4
Akahad1 <aksahad234567@gmail.com>
2025-01-16 18:54:50 +0600

    add problem10

--- problem5.js
+++ problem5.js
@@ -77,3 +77,15 @@ function longestWord(sentence) {
     );
 }
 console.log(longestWord("I love programming"));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+console.log(isBalanced("(())"));
