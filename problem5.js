16adeefeb43515337a2d4c4f422c45375a528e81
Akahad1 <aksahad234567@gmail.com>
2025-01-17 18:39:34 +0600

    add problem14

--- problem5.js
+++ problem5.js
@@ -91,3 +91,13 @@ function longestWord(sentence) {
 function removeDuplicates(arr) {
   return [...new Set(arr)];
 }
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
