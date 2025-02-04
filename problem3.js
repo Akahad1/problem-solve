6d5317dea672509e31ceb218539cef3d411fbd7c
Akahad1 <aksahad234567@gmail.com>
2025-02-04 21:49:59 +0600

    problem22

--- problem3.js
+++ problem3.js
@@ -41,3 +41,7 @@ function isBalanced(str) {
   return stack.length === 0;
 }
 console.log(isBalanced("(())"));
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
