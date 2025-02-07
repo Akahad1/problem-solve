49403453a90358a252b03f3a06d35e9554ba80da
Akahad1 <aksahad234567@gmail.com>
2025-02-07 20:36:11 +0600

    problem24

--- problem4.js
+++ problem4.js
@@ -15,17 +15,7 @@ console.log(mergeSorted([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
 function findIntersection(arr1, arr2) {
   return arr1.filter((num) => arr2.includes(num));
 }
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
+
 function longestWord(sentence) {
   return sentence
     .split(" ")
