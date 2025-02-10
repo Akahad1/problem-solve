b0a0f393a2ab665c13e25036ff568d7a79736154
Akahad1 <aksahad234567@gmail.com>
2025-02-10 21:54:06 +0600

    problem24

--- problem4.js
+++ problem4.js
@@ -36,3 +36,13 @@ function longestWord(sentence) {
       ""
     );
 }
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
