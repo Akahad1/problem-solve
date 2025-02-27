17f81b8c45a34877685a9cb8fa561952f2642237
Akahad1 <aksahad234567@gmail.com>
2025-02-27 10:04:47 +0600

    add 25 problem

--- problem5.js
+++ problem5.js
@@ -122,3 +122,12 @@ function longestWord(sentence) {
       ""
     );
 }
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
