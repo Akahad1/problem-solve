4a4786f52394d2669bdfb87e8137b50a032ec640
Akahad1 <aksahad234567@gmail.com>
2025-02-28 23:49:10 +0600

    add 25 problem

--- problem4.js
+++ problem4.js
@@ -7,3 +7,12 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
