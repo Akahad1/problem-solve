eddcd554a7e8d084c37a9826ccb4285859361eb1
Akahad1 <aksahad234567@gmail.com>
2025-03-07 19:08:16 +0600

    add 25 problem

--- problem1.js
+++ problem1.js
@@ -5,3 +5,27 @@ const persons = [
   { name: "Shkive", age: 20, gender: "male" },
   { name: "Piya", age: 20, gender: "Female" },
 ];
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const result = filterMale(persons);
+console.log(result);
