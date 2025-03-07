08c87073b5f48a013e18ecd64dde192d0c39e035
Akahad1 <aksahad234567@gmail.com>
2025-03-07 19:06:02 +0600

    add 25 problem

--- problem4.js
+++ problem4.js
@@ -122,3 +122,19 @@ function longestWord(sentence) {
       ""
     );
 }
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const result = filterMale(persons);
+console.log(result);
