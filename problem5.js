02328fbd8478f7f452f0dd6e4580bf870eb25673
Akahad1 <aksahad234567@gmail.com>
2025-03-08 16:08:27 +0600

    add 25 problem

--- problem5.js
+++ problem5.js
@@ -1,1 +1,25 @@
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
 
+console.log(sortArry(cars));
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const result = filterMale(persons);
+console.log(result);
