fa00472c615de98f341f352b5c0afee13c6a541c
Akahad1 <aksahad234567@gmail.com>
2025-03-08 16:07:20 +0600

    add 25 problem

--- problem5.js
+++ problem5.js
@@ -7,3 +7,19 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const result = filterMale(persons);
+console.log(result);
