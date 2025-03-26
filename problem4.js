b77f177f448ed8bf3bf27b96c05807e12068bc38
Akahad1 <aksahad234567@gmail.com>
2025-03-27 05:12:06 +0600

    add 25 problem

--- problem4.js
+++ problem4.js
@@ -65,4 +65,4 @@ function findIntersection(arr1, arr2) {
 function mergeSorted(arr1, arr2) {
   return [...arr1, ...arr2].sort((a, b) => a - b);
 }
-console.log(mergeSorted([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
+console.log(mergeSorted([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]MM
