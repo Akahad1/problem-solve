915b5f947b49269b2d0f42c5fef492e85036437f
Akahad1 <aksahad234567@gmail.com>
2025-03-27 05:13:42 +0600

    add 25 problem

--- problem4.js
+++ problem4.js
@@ -66,3 +66,88 @@ function mergeSorted(arr1, arr2) {
   return [...arr1, ...arr2].sort((a, b) => a - b);
 }
 console.log(mergeSorted([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]MM
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
+
+console.log(sortArry(cars));
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const result = filterMale(persons);
+console.log(result);
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
+
+const frequency = (arr) => {
+  return arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+console.log(frequency([1, 2, 2, 3, 3, 3])); // {1: 1, 2: 2, 3: 3}
+
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
+function capitalizeWords(sentence) {
+  return sentence.replace(/\b\w/g, (char) => char.toUpperCase());
+}
+console.log(capitalizeWords("hello world"));
+
+console.log(longestWord("I love programming"));
+
+console.log(findIntersection([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
