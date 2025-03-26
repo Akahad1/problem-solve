415ae06081c37634c636b9ce86aed15d021746c6
Akahad1 <aksahad234567@gmail.com>
2025-03-27 05:12:33 +0600

    add 25 problem

--- problem3.js
+++ problem3.js
@@ -6,3 +6,14 @@ function double(number) {
   const double = number * 2;
   return addFive(double);
 }
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
--- problem5.js
+++ problem5.js
@@ -68,14 +68,3 @@ function throttle(func, delay) {
     }
   };
 }
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
