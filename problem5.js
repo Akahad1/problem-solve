ff0fd8ba50fb7f159b85f6b8c44b89a0a89611fc
Akahad1 <aksahad234567@gmail.com>
2025-03-27 05:13:06 +0600

    add 25 problem

--- problem5.js
+++ problem5.js
@@ -68,3 +68,18 @@ function throttle(func, delay) {
     }
   };
 }
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
