f0186effb9562f3018e96ecc5906f26a85d8d07b
Akahad1 <aksahad234567@gmail.com>
2025-04-07 19:33:13 +0600

    add 25 problem

--- problem1.js
+++ problem1.js
@@ -26,3 +26,40 @@ function addFive(number) {
   const add5 = number + 5;
   return add5;
 }
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "Gatsby", author: "George Orwell", year: 1949 },
+  { title: "Moby Dick", author: "Herman Melville", year: 1851 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+];
+
+function booksTitel(bookArry) {
+  const newArry = bookArry.map((book) => book.title);
+  return newArry;
+}
+
+const result = booksTitel(books);
+console.log(result);
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function mergeSorted(arr1, arr2) {
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+console.log(mergeSorted([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]MM
