e3cb6d289b09e5d4f0df1ebbf8f7e952e689ead0
Akahad1 <aksahad234567@gmail.com>
2025-04-07 19:31:59 +0600

    add 25 problem

--- problem1.js
+++ problem1.js
@@ -26,35 +26,3 @@ function addFive(number) {
   const add5 = number + 5;
   return add5;
 }
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function throttle(func, delay) {
-  let lastCall = 0;
-  return (...args) => {
-    const now = Date.now();
-    if (now - lastCall >= delay) {
-      lastCall = now;
-      func(...args);
-    }
-  };
-}
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
