be8b147c0bccb57832353b3abf308c4086e25b89
Akahad1 <aksahad234567@gmail.com>
2025-04-07 19:32:28 +0600

    add 25 problem

--- problem3.js
+++ problem3.js
@@ -2,3 +2,18 @@ function square(number) {
   const squared = number * number;
   return double(squared);
 }
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
--- problem4.js
+++ problem4.js
@@ -15,18 +15,3 @@ function throttle(func, delay) {
     }
   };
 }
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
