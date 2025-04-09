8e0a55f732119dd672fd83f103cf0a3991647d43
Akahad1 <aksahad234567@gmail.com>
2025-04-09 17:14:56 +0600

    add 25 problem

--- problem5.js
+++ problem5.js
@@ -38,3 +38,34 @@ console.log(capitalizeWords("hello world"));
 console.log(longestWord("I love programming"));
 
 console.log(findIntersection([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
+const people = [
+  { name: "Sahad", age: 25, gender: "male" },
+  { name: "Riya", age: 22, gender: "female" },
+  { name: "Alex", age: 30, gender: "male" },
+  { name: "piya", age: 28, gender: "female" },
+  { name: "David", age: 35, gender: "male" },
+];
+
+function modifyAgeByName(peopleArray, personName, newAge) {
+  const person = peopleArray.find((p) => p.name === personName);
+
+  if (person) {
+    person.age = newAge;
+  }
+
+  return peopleArray;
+}
+const updatedPeople = modifyAgeByName(people, "piya", 32);
+console.log(updatedPeople);
+const uniqueNumbers = (arr) => {
+  const freq = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freq)
+    .filter((key) => freq[key] === 1)
+    .map(Number);
+};
+
+console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
