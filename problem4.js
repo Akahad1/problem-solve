6d5ba41bdf98a34e25c02ab4f931b5ee3685a1fa
Akahad1 <aksahad234567@gmail.com>
2025-04-10 20:47:57 +0600

    add 25 proble

--- problem4.js
+++ problem4.js
@@ -26,27 +26,3 @@ const people = [
   { name: "piya", age: 28, gender: "female" },
   { name: "David", age: 35, gender: "male" },
 ];
-
-function modifyAgeByName(peopleArray, personName, newAge) {
-  const person = peopleArray.find((p) => p.name === personName);
-
-  if (person) {
-    person.age = newAge;
-  }
-
-  return peopleArray;
-}
-const updatedPeople = modifyAgeByName(people, "piya", 32);
-console.log(updatedPeople);
-const uniqueNumbers = (arr) => {
-  const freq = arr.reduce((acc, num) => {
-    acc[num] = (acc[num] || 0) + 1;
-    return acc;
-  }, {});
-
-  return Object.keys(freq)
-    .filter((key) => freq[key] === 1)
-    .map(Number);
-};
-
-console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
