d82db7ce1ea9c0b50d80f826b0423683ddb9e3d0
Akahad1 <aksahad234567@gmail.com>
2025-04-14 11:34:00 +0600

    add 25 proble

--- problem5.js
+++ problem5.js
@@ -35,3 +35,41 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
+const persons = [
+  { name: "Rakib", age: 20, gender: "male" },
+  { name: "Ria", age: 20, gender: "Female" },
+  { name: "Nokive", age: 20, gender: "male" },
+  { name: "Shkive", age: 20, gender: "male" },
+  { name: "Piya", age: 20, gender: "Female" },
+];
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
+
+console.log(sortArry(cars));
