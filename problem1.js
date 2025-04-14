6f66f161db799a6eb02fa1a38b0baeb0ea4bbda4
Akahad1 <aksahad234567@gmail.com>
2025-04-14 11:53:10 +0600

    add 25 proble

--- problem1.js
+++ problem1.js
@@ -73,3 +73,22 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "Gatsby", author: "George Orwell", year: 1949 },
+  { title: "Moby Dick", author: "Herman Melville", year: 1851 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+];
+
+function booksTitel(bookArry) {
+  const newArry = bookArry.map((book) => book.title);
+  return newArry;
+}
+
+const result = booksTitel(books);
+console.log(result);
