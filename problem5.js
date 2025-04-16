8dd0df181d5aa67fe8471a106e3e8e5030ba8eea
Akahad1 <aksahad234567@gmail.com>
2025-04-16 18:45:53 +0600

    add 25 proble

--- problem5.js
+++ problem5.js
@@ -69,3 +69,25 @@ function squareDoubleAddFive(number) {
 }
 
 console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
