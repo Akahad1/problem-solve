ca891072804d52b862ee7f7c8b1fb6e2d1504553
Akahad1 <aksahad234567@gmail.com>
2025-04-22 13:59:33 +0600

    add

---
+++ problem1.js
@@ -0,0 +1,59 @@
+function addFive(number) {
+  const add5 = number + 5;
+  return add5;
+}
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "Gatsby", author: "George Orwell", year: 1949 },
+  { title: "Moby Dick", author: "Herman Melville", year: 1851 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+];
+
+function booksTitel(bookArry) {
+  const newArry = bookArry.map((book) => book.title);
+  return newArry;
+}
+
+const result = booksTitel(books);
+console.log(result);
+const people = [
+  { name: "Sahad", age: 25, gender: "male" },
+  { name: "Riya", age: 22, gender: "female" },
+  { name: "Alex", age: 30, gender: "male" },
+  { name: "piya", age: 28, gender: "female" },
+  { name: "David", age: 35, gender: "male" },
+];
+const frequency = (arr) => {
+  return arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+console.log(frequency([1, 2, 2, 3, 3, 3])); // {1: 1, 2: 2, 3: 3}
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
