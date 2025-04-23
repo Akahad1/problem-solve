1c4ff4eaeb95c3210f3b069a2966b301dccbb8f6
Akahad1 <aksahad234567@gmail.com>
2025-04-23 22:01:46 +0600

    add

--- problem5.js
+++ problem5.js
@@ -33,3 +33,59 @@ const uniqueNumbers = (arr) => {
 };
 
 console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
+function booksTitel(bookArry) {
+  const newArry = bookArry.map((book) => book.title);
+  return newArry;
+}
+
+const result = booksTitel(books);
+console.log(result);
+const people = [
+  { name: "Sahad", age: 25, gender: "male" },
+  { name: "Riya", age: 22, gender: "female" },
+  { name: "Alex", age: 30, gender: "male" },
+  { name: "piya", age: 28, gender: "female" },
+  { name: "David", age: 35, gender: "male" },
+];
+const frequency = (arr) => {
+  return arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+console.log(frequency([1, 2, 2, 3, 3, 3])); // {1: 1, 2: 2, 3: 3}
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
+
+console.log(sortArry(cars));
