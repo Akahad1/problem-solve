a03954b7abe1cfeb69958785bdff2fb2b641fca3
Akahad1 <aksahad234567@gmail.com>
2025-04-29 19:51:13 +0600

    add

---
+++ problem4.js
@@ -0,0 +1,22 @@
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
--- problem5.js
+++ problem5.js
@@ -49,25 +49,3 @@ function squareDoubleAddFive(number) {
 }
 
 console.log(squareDoubleAddFive(5));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function throttle(func, delay) {
-  let lastCall = 0;
-  return (...args) => {
-    const now = Date.now();
-    if (now - lastCall >= delay) {
-      lastCall = now;
-      func(...args);
-    }
-  };
-}
