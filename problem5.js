d8a5184903d6d63face195884a7f0570618dd1e8
Akahad1 <aksahad234567@gmail.com>
2025-05-03 21:25:42 +0600

    add

--- problem5.js
+++ problem5.js
@@ -44,3 +44,30 @@ function double(number) {
   const double = number * 2;
   return addFive(double);
 }
+function modifyAgeByName(peopleArray, personName, newAge) {
+  const person = peopleArray.find((p) => p.name === personName);
+
+  if (person) {
+    person.age = newAge;
+  }
+
+  return peopleArray;
+}
+const updatedPeople = modifyAgeByName(people, "piya", 32);
+console.log(updatedPeople);
+const uniqueNumbers = (arr) => {
+  const freq = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freq)
+    .filter((key) => freq[key] === 1)
+    .map(Number);
+};
+
+console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
