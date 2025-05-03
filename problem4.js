888188033330867e05976cd72e40904189f4dceb
Akahad1 <aksahad234567@gmail.com>
2025-05-03 21:25:29 +0600

    add

--- problem4.js
+++ problem4.js
@@ -25,23 +25,3 @@ function square(number) {
   const squared = number * number;
   return double(squared);
 }
-const persons = [
-  { name: "Rakib", age: 20, gender: "male" },
-  { name: "Ria", age: 20, gender: "Female" },
-  { name: "Nokive", age: 20, gender: "male" },
-  { name: "Shkive", age: 20, gender: "male" },
-  { name: "Piya", age: 20, gender: "Female" },
-];
-function longestWord(sentence) {
-  return sentence
-    .split(" ")
-    .reduce(
-      (longest, word) => (word.length > longest.length ? word : longest),
-      ""
-    );
-}
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-
--- problem5.js
+++ problem5.js
@@ -25,3 +25,22 @@ function throttle(func, delay) {
     }
   };
 }
+const persons = [
+  { name: "Rakib", age: 20, gender: "male" },
+  { name: "Ria", age: 20, gender: "Female" },
+  { name: "Nokive", age: 20, gender: "male" },
+  { name: "Shkive", age: 20, gender: "male" },
+  { name: "Piya", age: 20, gender: "Female" },
+];
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
