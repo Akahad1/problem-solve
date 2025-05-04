74c059d79af92d41d57d8649766fe9a0362f20a6
Akahad1 <aksahad234567@gmail.com>
2025-05-04 19:03:36 +0600

    add

--- problem4.js
+++ problem4.js
@@ -15,32 +15,3 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
-function throttle(func, delay) {
-  let lastCall = 0;
-  return (...args) => {
-    const now = Date.now();
-    if (now - lastCall >= delay) {
-      lastCall = now;
-      func(...args);
-    }
-  };
-}
-const persons = [
-  { name: "Rakib", age: 20, gender: "male" },
-  { name: "Ria", age: 20, gender: "Female" },
-  { name: "Nokive", age: 20, gender: "male" },
-  { name: "Shkive", age: 20, gender: "male" },
-  { name: "Piya", age: 20, gender: "Female" },
-];
-function longestWord(sentence) {
-  return sentence
-    .split(" ")
-    .reduce(
-      (longest, word) => (word.length > longest.length ? word : longest),
-      ""
-    );
-}
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
