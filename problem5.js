dd9d17117ecbe05157e39ccfea40e65b1d44c229
Akahad1 <aksahad234567@gmail.com>
2025-05-06 11:11:46 +0600

    add

---
+++ problem5.js
@@ -0,0 +1,17 @@
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
