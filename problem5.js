df323e8ace3cd7e5c6a74b0d5e229f3e5e457642
Akahad1 <aksahad234567@gmail.com>
2025-05-09 11:10:06 +0600

    add

--- problem5.js
+++ problem5.js
@@ -21,3 +21,28 @@ const uniqueNumbers = (arr) => {
 };
 
 console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+const persons = [
+  { name: "Rakib", age: 20, gender: "male" },
+  { name: "Ria", age: 20, gender: "Female" },
+  { name: "Nokive", age: 20, gender: "male" },
+  { name: "Shkive", age: 20, gender: "male" },
+  { name: "Piya", age: 20, gender: "Female" },
+];
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
