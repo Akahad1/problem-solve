05310e8f65bdac1259e99b9bdf546c38f59b5c30
Akahad1 <aksahad234567@gmail.com>
2025-05-10 21:13:17 +0600

    add

--- problem5.js
+++ problem5.js
@@ -1,21 +1,50 @@
 function squareDoubleAddFive(number) {
-    return square(number);
-  }
-  
-  console.log(squareDoubleAddFive(5));
-  function removeDuplicates(arr) {
-    return [...new Set(arr)];
-  }
-  
-  console.log(isBalanced("(())"));
-  function filterMale(peopleArray) {
-    const male = peopleArray.filter((person) => person.gender !== "Female");
-    const maleName = male.map((person) => person.name);
-    return maleName;
-  }
-  const results = filterMale(persons);
-  console.log(result);
-  function square(number) {
-    const squared = number * number;
-    return double(squared);
-  }
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+const persons = [
+  { name: "Rakib", age: 20, gender: "male" },
+  { name: "Ria", age: 20, gender: "Female" },
+  { name: "Nokive", age: 20, gender: "male" },
+  { name: "Shkive", age: 20, gender: "male" },
+  { name: "Piya", age: 20, gender: "Female" },
+];
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
