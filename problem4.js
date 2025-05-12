86a198613eef935a018c02876c468f28e7d38b94
Akahad1 <aksahad234567@gmail.com>
2025-05-12 17:08:37 +0600

    add

--- problem4.js
+++ problem4.js
@@ -19,28 +19,3 @@ function square(number) {
   const squared = number * number;
   return double(squared);
 }
-function throttle(func, delay) {
-  let lastCall = 0;
-  return (...args) => {
-    const now = Date.now();
-    if (now - lastCall >= delay) {
-      lastCall = now;
-      func(...args);
-    }
-  };
-}
-const persons = [
-  { name: "Rakib", age: 20, gender: "male" },
-  { name: "Ria", age: 20, gender: "Female" },
-  { name: "Nokive", age: 20, gender: "male" },
-  { name: "Shkive", age: 20, gender: "male" },
-  { name: "Piya", age: 20, gender: "Female" },
-];
-function longestWord(sentence) {
-  return sentence
-    .split(" ")
-    .reduce(
-      (longest, word) => (word.length > longest.length ? word : longest),
-      ""
-    );
-}
