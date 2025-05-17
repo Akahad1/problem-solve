afe2f02b49493243f45aba61542d20f1abc73ccb
Akahad1 <aksahad234567@gmail.com>
2025-05-17 19:51:11 +0600

    add

--- problem5.js
+++ problem5.js
@@ -51,24 +51,3 @@ function double(number) {
   const double = number * 2;
   return addFive(double);
 }
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function square(number) {
-  const squared = number * number;
-  return double(squared);
-}
