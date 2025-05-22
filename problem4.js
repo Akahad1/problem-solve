e433868168722bd45edf738eea25328eb232cf37
Akahad1 <aksahad234567@gmail.com>
2025-05-22 20:44:56 +0600

    add

--- problem4.js
+++ problem4.js
@@ -45,3 +45,30 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
