9391f81e60642bc32b5f05a49234a7a3d8602840
Akahad1 <aksahad234567@gmail.com>
2025-05-23 12:20:14 +0600

    add

--- problem4.js
+++ problem4.js
@@ -58,3 +58,17 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
