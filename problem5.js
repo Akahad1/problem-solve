49dbe2bc362867e2a7c115e20b872c55ea1422eb
Akahad1 <aksahad234567@gmail.com>
2025-05-26 11:02:17 +0600

    add

--- problem5.js
+++ problem5.js
@@ -49,26 +49,3 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
-function findIntersection(arr1, arr2) {
-  return arr1.filter((num) => arr2.includes(num));
-}
-const checkNumber = (num) => {
-  if (num > 0) return "Positive";
-  if (num < 0) return "Negative";
-  return "Zero";
-};
-
-console.log(checkNumber(5)); // "Positive"
-console.log(checkNumber(-3)); // "Negative"
-console.log(checkNumber(0)); // "Zero"
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
