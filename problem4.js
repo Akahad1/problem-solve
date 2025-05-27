1e1d869d5e9f6f7ea6f691d033360b970b699aa1
Akahad1 <aksahad234567@gmail.com>
2025-05-27 21:32:24 +0600

    add

---
+++ problem4.js
@@ -0,0 +1,18 @@
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
