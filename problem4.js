42c6bfc34a7dbfebd8ae22f8eacbfacbd34a1b21
Akahad1 <aksahad234567@gmail.com>
2025-05-29 11:24:50 +0600

    add

--- problem4.js
+++ problem4.js
@@ -54,21 +54,3 @@ const secondMax = (arr) => {
 console.log(secondMax([10, 20, 30, 40])); // 30
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
-function factorial(n) {
-  return n === 0 ? 1 : n * factorial(n - 1);
-}
-console.log(factorial(5)); // Output: 120
-const cars = [
-  { make: "Toyota", model: "Corolla", year: 2015 },
-  { make: "Honda", model: "Civic", year: 2018 },
-  { make: "Ford", model: "Mustang", year: 2012 },
-  { make: "Chevrolet", model: "Camaro", year: 2016 },
-  { make: "BMW", model: "3 Series", year: 2019 },
-];
-
-console.log(sortArry(cars));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
