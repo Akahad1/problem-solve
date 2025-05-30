6061782bf718f84139e0f3ea1bd74079c2fa4d3f
Akahad1 <aksahad234567@gmail.com>
2025-05-30 20:55:10 +0600

    add

---
+++ problem5.js
@@ -0,0 +1,20 @@
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
