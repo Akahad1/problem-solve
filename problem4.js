c36d938666aced1d7c5dda57fc868503ba8ce982
Akahad1 <aksahad234567@gmail.com>
2025-06-01 12:18:31 +0600

    add

--- problem4.js
+++ problem4.js
@@ -30,28 +30,3 @@ const checkNumber = (num) => {
 console.log(checkNumber(5)); // "Positive"
 console.log(checkNumber(-3)); // "Negative"
 console.log(checkNumber(0)); // "Zero"
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function factorial(n) {
-  return n === 0 ? 1 : n * factorial(n - 1);
-}
-console.log(factorial(5)); // Output: 120
-const cars = [
-  { make: "Toyota", model: "Corolla", year: 2015 },
-  { make: "Honda", model: "Civic", year: 2018 },
-  { make: "Ford", model: "Mustang", year: 2012 },
-  { make: "Chevrolet", model: "Camaro", year: 2016 },
-  { make: "BMW", model: "3 Series", year: 2019 },
-];
-
-console.log(sortArry(cars));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
--- problem5.js
+++ problem5.js
@@ -15,3 +15,28 @@ function sortArry(cars) {
   const ascendingYear = cars.sort((a, b) => a.year - b.year);
   return ascendingYear;
 }
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
+
+console.log(sortArry(cars));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
