7542cadd9b7911ddc8a8439b3ae61ca8fbdbea80
Akahad1 <aksahad234567@gmail.com>
2025-06-02 21:23:59 +0600

    add

--- problem4.js
+++ problem4.js
@@ -51,24 +51,3 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-function findIntersection(arr1, arr2) {
-  return arr1.filter((num) => arr2.includes(num));
-}
-const checkNumber = (num) => {
-  if (num > 0) return "Positive";
-  if (num < 0) return "Negative";
-  return "Zero";
-};
-
-console.log(checkNumber(5)); // "Positive"
-console.log(checkNumber(-3)); // "Negative"
-console.log(checkNumber(0)); // "Zero"
