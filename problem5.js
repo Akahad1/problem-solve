fcad4b3500e552826deff989cc7593292780ed4f
Akahad1 <aksahad234567@gmail.com>
2025-06-02 21:24:12 +0600

    add

--- problem5.js
+++ problem5.js
@@ -19,3 +19,14 @@ const checkNumber = (num) => {
 console.log(checkNumber(5)); // "Positive"
 console.log(checkNumber(-3)); // "Negative"
 console.log(checkNumber(0)); // "Zero"
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
