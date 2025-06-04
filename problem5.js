96a43d07817bc2ca4918a09d15d1e5746006cd03
Akahad1 <aksahad234567@gmail.com>
2025-06-04 12:43:15 +0600

    add

--- problem5.js
+++ problem5.js
@@ -22,30 +22,3 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
-function factorial(n) {
-  return n === 0 ? 1 : n * factorial(n - 1);
-}
-console.log(factorial(5)); // Output: 120
-const cars = [
-  { make: "Toyota", model: "Corolla", year: 2015 },
-  { make: "Honda", model: "Civic", year: 2018 },
-  { make: "Ford", model: "Mustang", year: 2012 },
-  { make: "Chevrolet", model: "Camaro", year: 2016 },
-  { make: "BMW", model: "3 Series", year: 2019 },
-];
-
-console.log(sortArry(cars));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
