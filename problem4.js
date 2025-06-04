cf9d8af0f52fcb1c0d934457a76a35b7ebfd9e14
Akahad1 <aksahad234567@gmail.com>
2025-06-04 12:42:59 +0600

    add

---
+++ problem4.js
@@ -0,0 +1,23 @@
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
