d75cc2174a8ff7992937b582b41a902e6c8bebd2
Akahad1 <aksahad234567@gmail.com>
2025-06-06 10:10:59 +0600

    add

--- problem5.js
+++ problem5.js
@@ -27,3 +27,19 @@ function removeDuplicates(arr) {
 }
 
 console.log(isBalanced("(())"));
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
