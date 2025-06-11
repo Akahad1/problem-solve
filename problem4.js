6ad4c7418c0cdc3b06e34156feee3dffa021bc95
Akahad1 <aksahad234567@gmail.com>
2025-06-11 21:35:28 +0600

    add

--- problem4.js
+++ problem4.js
@@ -57,18 +57,3 @@ function square(number) {
   const squared = number * number;
   return double(squared);
 }
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
