edbdf3d8ae9b8ea7b004be2cdb16c55463e586a8
Akahad1 <aksahad234567@gmail.com>
2025-06-13 12:14:46 +0600

    add

--- problem4.js
+++ problem4.js
@@ -56,19 +56,3 @@ const secondMax = (arr) => {
 console.log(secondMax([10, 20, 30, 40])); // 30
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
---
+++ problem5.js
@@ -0,0 +1,16 @@
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
