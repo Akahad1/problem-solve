e354ca9b61821118f6082bec23ff923247cc673a
Akahad1 <aksahad234567@gmail.com>
2025-06-16 12:14:34 +0600

    add

--- problem4.js
+++ problem4.js
@@ -37,22 +37,3 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
-function findIntersection(arr1, arr2) {
-  return arr1.filter((num) => arr2.includes(num));
-}
--- problem5.js
+++ problem5.js
@@ -14,3 +14,22 @@ function squareDoubleAddFive(number) {
 }
 
 console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
