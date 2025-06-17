c3c8e2dc647499cff4a8bd5e88f619ff3f05ee5f
Akahad1 <aksahad234567@gmail.com>
2025-06-17 13:51:11 +0600

    add

--- problem5.js
+++ problem5.js
@@ -40,3 +40,35 @@ function sortArry(cars) {
   const ascendingYear = cars.sort((a, b) => a.year - b.year);
   return ascendingYear;
 }
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
