7c396d67bd77a231dfd8e6f45074d07000f91e43
Akahad1 <aksahad234567@gmail.com>
2025-06-17 13:51:00 +0600

    add

--- problem5.js
+++ problem5.js
@@ -24,3 +24,19 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
