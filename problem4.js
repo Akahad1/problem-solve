3554155c741a87f333b34197f92b55a93a81a96c
Akahad1 <aksahad234567@gmail.com>
2025-06-25 12:38:51 +0600

    add

---
+++ problem4.js
@@ -0,0 +1,10 @@
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
--- problem5.js
+++ problem5.js
@@ -62,13 +62,3 @@ function sortArry(cars) {
   const ascendingYear = cars.sort((a, b) => a.year - b.year);
   return ascendingYear;
 }
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
