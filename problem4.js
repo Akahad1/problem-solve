ec5ddb60021af09b3666dfaed81961c817ac6ae3
Akahad1 <aksahad234567@gmail.com>
2025-06-27 10:36:57 +0600

    add

---
+++ problem4.js
@@ -0,0 +1,18 @@
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
