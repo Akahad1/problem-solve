62ee2fd0966ff5e2abad47fd5f6166e92ad2a8e7
Akahad1 <aksahad234567@gmail.com>
2025-06-28 21:00:14 +0600

    add

--- problem4.js
+++ problem4.js
@@ -41,3 +41,34 @@ function removeDuplicates(arr) {
 }
 
 console.log(isBalanced("(())"));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
