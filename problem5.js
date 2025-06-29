57c18260c1f36e18f1b907af9c75f2ca08d307c6
Akahad1 <aksahad234567@gmail.com>
2025-06-29 21:20:40 +0600

    add

--- problem5.js
+++ problem5.js
@@ -52,23 +52,3 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function square(number) {
-  const squared = number * number;
-  return double(squared);
-}
-const checkNumber = (num) => {
-  if (num > 0) return "Positive";
-  if (num < 0) return "Negative";
-  return "Zero";
-};
-
-console.log(checkNumber(5)); // "Positive"
-console.log(checkNumber(-3)); // "Negative"
-console.log(checkNumber(0)); // "Zero"
