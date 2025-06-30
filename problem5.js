2e00fcfdf3b4915e1f5bfef6488cf81a55cb8515
Akahad1 <aksahad234567@gmail.com>
2025-06-30 12:28:02 +0600

    add

--- problem5.js
+++ problem5.js
@@ -34,20 +34,3 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-const secondMax = (arr) => {
-  const uniqueArr = [...new Set(arr)];
-  uniqueArr.sort((a, b) => b - a);
-  return uniqueArr[1];
-};
-
-console.log(secondMax([10, 20, 30, 40])); // 30
-
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
