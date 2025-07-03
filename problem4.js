7bb885aecb2ebe13c8cdfb33dcf3a1c772a00ec4
Akahad1 <aksahad234567@gmail.com>
2025-07-03 21:31:31 +0600

    add

--- problem4.js
+++ problem4.js
@@ -53,3 +53,22 @@ const checkNumber = (num) => {
 console.log(checkNumber(5)); // "Positive"
 console.log(checkNumber(-3)); // "Negative"
 console.log(checkNumber(0)); // "Zero"
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
