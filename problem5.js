e729170490771b7ad4727b9da0b987bbd894c50b
Akahad1 <aksahad234567@gmail.com>
2025-07-13 21:41:14 +0600

    add

--- problem5.js
+++ problem5.js
@@ -56,19 +56,3 @@ console.log(isBalanced("(())"));
 function findIntersection(arr1, arr2) {
   return arr1.filter((num) => arr2.includes(num));
 }
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
