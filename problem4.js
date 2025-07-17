614674378cda1bec5d6111142dba0f0c17c21c1d
Akahad1 <aksahad234567@gmail.com>
2025-07-17 12:46:48 +0600

    add

--- problem4.js
+++ problem4.js
@@ -30,3 +30,19 @@ console.log(isBalanced("(())"));
 function findIntersection(arr1, arr2) {
   return arr1.filter((num) => arr2.includes(num));
 }
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
