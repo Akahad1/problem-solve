7ecb0d290bf9d1893bc39fefd95502caf525c562
Akahad1 <aksahad234567@gmail.com>
2025-07-25 19:08:13 +0600

    add

--- problem4.js
+++ problem4.js
@@ -34,17 +34,3 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
-function findIntersection(arr1, arr2) {
-  return arr1.filter((num) => arr2.includes(num));
-}
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
--- problem5.js
+++ problem5.js
@@ -22,3 +22,17 @@ const checkNumber = (num) => {
 console.log(checkNumber(5)); // "Positive"
 console.log(checkNumber(-3)); // "Negative"
 console.log(checkNumber(0)); // "Zero"
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
