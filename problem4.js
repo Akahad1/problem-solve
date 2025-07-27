fd45c32a78dbc50f4cd399ed4fb97bfd4973d1fb
Akahad1 <aksahad234567@gmail.com>
2025-07-27 11:50:52 +0600

    add

--- problem4.js
+++ problem4.js
@@ -25,19 +25,3 @@ console.log(checkNumber(0)); // "Zero"
 function findIntersection(arr1, arr2) {
   return arr1.filter((num) => arr2.includes(num));
 }
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
--- problem5.js
+++ problem5.js
@@ -29,3 +29,19 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
