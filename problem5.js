fc6abff27f3dedba9ab89ba1af71ffd6cf9ac0f5
Akahad1 <aksahad234567@gmail.com>
2025-07-31 20:13:47 +0600

    solved: added a new solution

---
+++ problem5.js
@@ -0,0 +1,15 @@
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
