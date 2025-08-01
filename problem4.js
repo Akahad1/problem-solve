a780a31b47d45f29a6bd73510f187945e408bae6
Akahad1 <aksahad234567@gmail.com>
2025-08-01 10:59:13 +0600

    add

--- problem4.js
+++ problem4.js
@@ -24,29 +24,3 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
-const secondMax = (arr) => {
-  const uniqueArr = [...new Set(arr)];
-  uniqueArr.sort((a, b) => b - a);
-  return uniqueArr[1];
-};
-
-console.log(secondMax([10, 20, 30, 40])); // 30
-
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
-function square(number) {
-  const squared = number * number;
-  return double(squared);
-}
-const checkNumber = (num) => {
-  if (num > 0) return "Positive";
-  if (num < 0) return "Negative";
-  return "Zero";
-};
-
-console.log(checkNumber(5)); // "Positive"
-console.log(checkNumber(-3)); // "Negative"
-console.log(checkNumber(0)); // "Zero"
