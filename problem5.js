2cfa09f95332098c477345bb11d680150149e630
Akahad1 <aksahad234567@gmail.com>
2025-08-04 20:30:32 +0600

    add

--- problem5.js
+++ problem5.js
@@ -52,3 +52,23 @@ console.log(checkNumber(0)); // "Zero"
 function findIntersection(arr1, arr2) {
   return arr1.filter((num) => arr2.includes(num));
 }
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
