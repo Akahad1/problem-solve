3791d7169dba487692cf2d4ca4929cef0d314550
Akahad1 <aksahad234567@gmail.com>
2025-08-09 19:53:34 +0600

    add

--- problem4.js
+++ problem4.js
@@ -16,30 +16,3 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
-const secondMax = (arr) => {
-  const uniqueArr = [...new Set(arr)];
-  uniqueArr.sort((a, b) => b - a);
-  return uniqueArr[1];
-};
-
-console.log(secondMax([10, 20, 30, 40])); // 30
-
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
-function square(number) {
-  const squared = number * number;
-  return double(squared);
-}
-const checkNumber = (num) => {
-  if (num > 0) return "Positive";
-  if (num < 0) return "Negative";
-  return "Zero";
-};
-
-console.log(checkNumber(5)); // "Positive"
-console.log(checkNumber(-3)); // "Negative"
-console.log(checkNumber(0)); // "Zero"
-
