f60b4755e0d82cecc09d87f92f36c6a3303bab44
Akahad1 <aksahad234567@gmail.com>
2025-08-10 21:01:39 +0600

    add

--- problem5.js
+++ problem5.js
@@ -29,3 +29,31 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
