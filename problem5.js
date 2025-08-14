365a875087c5554ecb7b115fb1a295274782a677
Akahad1 <aksahad234567@gmail.com>
2025-08-14 19:35:40 +0600

    add

--- problem5.js
+++ problem5.js
@@ -57,3 +57,18 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
