79cf2cde53dff9adb9534cebcc25746ed308a66e
Akahad1 <aksahad234567@gmail.com>
2025-08-18 21:58:10 +0600

    add

---
+++ problem5.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
+
+console.log(sortArry(cars));
