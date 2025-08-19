f54c31eed509de744ed9fa00149d35294ed1c308
Akahad1 <aksahad234567@gmail.com>
2025-08-19 22:08:46 +0600

    add

--- problem4.js
+++ problem4.js
@@ -52,23 +52,4 @@ const checkNumber = (num) => {
 console.log(checkNumber(5)); // "Positive"
 console.log(checkNumber(-3)); // "Negative"
 console.log(checkNumber(0)); // "Zero"
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
 
-console.log(isBalanced("(())"));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
