5902ebf363f1f5df8768884b131c7a3a27a94a75
Akahad1 <aksahad234567@gmail.com>
2025-08-19 22:09:07 +0600

    add

--- problem4.js
+++ problem4.js
@@ -39,17 +39,3 @@ function squareDoubleAddFive(number) {
 }
 
 console.log(squareDoubleAddFive(5));
-function square(number) {
-  const squared = number * number;
-  return double(squared);
-}
-const checkNumber = (num) => {
-  if (num > 0) return "Positive";
-  if (num < 0) return "Negative";
-  return "Zero";
-};
-
-console.log(checkNumber(5)); // "Positive"
-console.log(checkNumber(-3)); // "Negative"
-console.log(checkNumber(0)); // "Zero"
-
