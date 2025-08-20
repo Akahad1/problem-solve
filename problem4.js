f6f9125f9fa41d7198ea0e0ec6f8a41aa3c3643a
Akahad1 <aksahad234567@gmail.com>
2025-08-20 19:41:26 +0600

    add

--- problem4.js
+++ problem4.js
@@ -57,18 +57,3 @@ const secondMax = (arr) => {
 console.log(secondMax([10, 20, 30, 40])); // 30
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function findIntersection(arr1, arr2) {
-  return arr1.filter((num) => arr2.includes(num));
-}
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
