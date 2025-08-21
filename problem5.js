f18cd095251d8ea1994a669c1f7dd069d869455f
Akahad1 <aksahad234567@gmail.com>
2025-08-21 10:37:50 +0600

    add

--- problem5.js
+++ problem5.js
@@ -44,3 +44,31 @@ function double(number) {
   const double = number * 2;
   return addFive(double);
 }
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
