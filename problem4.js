b59d741528fde533c0efd2108ed7c7664bdd4439
Akahad1 <aksahad234567@gmail.com>
2025-08-22 11:50:09 +0600

    add

--- problem4.js
+++ problem4.js
@@ -11,36 +11,3 @@ const checkNumber = (num) => {
 console.log(checkNumber(5)); // "Positive"
 console.log(checkNumber(-3)); // "Negative"
 console.log(checkNumber(0)); // "Zero"
-function factorial(n) {
-  return n === 0 ? 1 : n * factorial(n - 1);
-}
-console.log(factorial(5)); // Output: 120
-const cars = [
-  { make: "Toyota", model: "Corolla", year: 2015 },
-  { make: "Honda", model: "Civic", year: 2018 },
-  { make: "Ford", model: "Mustang", year: 2012 },
-  { make: "Chevrolet", model: "Camaro", year: 2016 },
-  { make: "BMW", model: "3 Series", year: 2019 },
-];
-
-console.log(sortArry(cars));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
-function double(number) {
-  const double = number * 2;
-  return addFive(double);
-}
