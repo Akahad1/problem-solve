c403d6b0e7abd891b17ef931a4c79445776e3a39
Akahad1 <aksahad234567@gmail.com>
2025-08-25 22:21:17 +0600

    add

--- problem5.js
+++ problem5.js
@@ -18,3 +18,28 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
+function double(number) {
+  const double = number * 2;
+  return addFive(double);
+}
+function square(number) {
+  const squared = number * number;
+  return double(squared);
+}
+const checkNumber = (num) => {
+  if (num > 0) return "Positive";
+  if (num < 0) return "Negative";
+  return "Zero";
+};
+
+console.log(checkNumber(5)); // "Positive"
+console.log(checkNumber(-3)); // "Negative"
+console.log(checkNumber(0)); // "Zero"
+function findIntersection(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
+function squareDoubleAddFive(number) {
+  return square(number);
+}
+
+console.log(squareDoubleAddFive(5));
