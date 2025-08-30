577b194c5fc8c44fb44294163d4384b0ab4a50eb
Akahad1 <aksahad234567@gmail.com>
2025-08-30 11:23:48 +0600

    add

---
+++ problem5.js
@@ -0,0 +1,16 @@
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
