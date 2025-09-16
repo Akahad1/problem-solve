8d32903a3214cfb13ac825f84a5411ae8ede88a6
Akahad1 <aksahad234567@gmail.com>
2025-09-16 10:26:06 +0600

    add

--- problem5.js
+++ problem5.js
@@ -36,3 +36,14 @@ function squareDoubleAddFive(number) {
 }
 
 console.log(squareDoubleAddFive(5));
+function isBalanced(str) {
+  const stack = [];
+  for (let char of str) {
+    if (char === "(") stack.push(char);
+    else if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
