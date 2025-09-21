a1d5f3f37d4bd8443a9cf3b3083046a32185858d
Akahad1 <aksahad234567@gmail.com>
2025-09-21 20:36:23 +0600

    add

--- problem4.js
+++ problem4.js
@@ -18,3 +18,21 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
+const secondMax = (arr) => {
+  const uniqueArr = [...new Set(arr)];
+  uniqueArr.sort((a, b) => b - a);
+  return uniqueArr[1];
+};
+
+console.log(secondMax([10, 20, 30, 40])); // 30
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
