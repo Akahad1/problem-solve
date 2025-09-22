c5f6b21b0a99dd66f862a2e39c81cdb4e0f1abd7
Akahad1 <aksahad234567@gmail.com>
2025-09-22 21:04:16 +0600

    add

--- problem4.js
+++ problem4.js
@@ -23,3 +23,19 @@ function isBalanced(str) {
   }
   return stack.length === 0;
 }
+function filterMale(peopleArray) {
+  const male = peopleArray.filter((person) => person.gender !== "Female");
+  const maleName = male.map((person) => person.name);
+  return maleName;
+}
+const results = filterMale(persons);
+console.log(result);
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(isBalanced("(())"));
+function sortArry(cars) {
+  const ascendingYear = cars.sort((a, b) => a.year - b.year);
+  return ascendingYear;
+}
--- problem5.js
+++ problem5.js
@@ -11,19 +11,3 @@ const cars = [
 ];
 
 console.log(sortArry(cars));
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
