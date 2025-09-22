b871fb5e189ae8b9ae59dcf194a68d927c7842fe
Akahad1 <aksahad234567@gmail.com>
2025-09-22 21:04:25 +0600

    add

--- problem4.js
+++ problem4.js
@@ -39,3 +39,16 @@ function sortArry(cars) {
   const ascendingYear = cars.sort((a, b) => a.year - b.year);
   return ascendingYear;
 }
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2012 },
+  { make: "Chevrolet", model: "Camaro", year: 2016 },
+  { make: "BMW", model: "3 Series", year: 2019 },
+];
+
+console.log(sortArry(cars));
