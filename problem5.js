ee27185c69302161ef336eab483e39ef180f13f0
Akahad1 <aksahad234567@gmail.com>
2025-09-26 22:02:37 +0600

    add

--- problem5.js
+++ problem5.js
@@ -36,19 +36,3 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
