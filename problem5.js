1a5b943b4f9cf03386dc3cf8f3994ce83493ba3e
Akahad1 <aksahad234567@gmail.com>
2025-10-06 20:17:06 +0600

    add

--- problem5.js
+++
@@ -1,9 +0,0 @@
-function sortArry(cars) {
-  const ascendingYear = cars.sort((a, b) => a.year - b.year);
-  return ascendingYear;
-}
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
