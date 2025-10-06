dae5cf6c768415806e3b310971130779925339c2
Akahad1 <aksahad234567@gmail.com>
2025-10-06 20:16:56 +0600

    add

--- problem5.js
+++ problem5.js
@@ -7,26 +7,3 @@ function removeDuplicates(arr) {
 }
 
 console.log(isBalanced("(())"));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
-function filterMale(peopleArray) {
-  const male = peopleArray.filter((person) => person.gender !== "Female");
-  const maleName = male.map((person) => person.name);
-  return maleName;
-}
-const results = filterMale(persons);
-console.log(result);
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
