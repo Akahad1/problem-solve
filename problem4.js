105b975dc835282907f8adb69d470392458c5c80
Akahad1 <aksahad234567@gmail.com>
2025-10-12 20:27:26 +0600

    add

--- problem4.js
+++ problem4.js
@@ -5,24 +5,3 @@ function filterMale(peopleArray) {
 }
 const results = filterMale(persons);
 console.log(result);
-function squareDoubleAddFive(number) {
-  return square(number);
-}
-
-console.log(squareDoubleAddFive(5));
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(isBalanced("(())"));
-function isBalanced(str) {
-  const stack = [];
-  for (let char of str) {
-    if (char === "(") stack.push(char);
-    else if (char === ")") {
-      if (stack.length === 0) return false;
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
