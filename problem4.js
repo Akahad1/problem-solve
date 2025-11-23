2be8021054aec2631354b29ee255637649f64e61
Akahad1 <aksahad234567@gmail.com>
2025-11-23 12:24:15 +0600

    add

---
+++ problem4.js
@@ -0,0 +1,43 @@
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
+function modifyAgeByName(peopleArray, personName, newAge) {
+  const person = peopleArray.find((p) => p.name === personName);
+
+  if (person) {
+    person.age = newAge;
+  }
+
+  return peopleArray;
+}
+const updatedPeople = modifyAgeByName(people, "piya", 32);
+console.log(updatedPeople);
+const uniqueNumbers = (arr) => {
+  const freq = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freq)
+    .filter((key) => freq[key] === 1)
+    .map(Number);
+};
+
+console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
+function capitalizeWords(sentence) {
+  return sentence.replace(/\b\w/g, (char) => char.toUpperCase());
+}
+console.log(capitalizeWords("hello world"));
+
+console.log(longestWord("I love programming"));
+
+console.log(findIntersection([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
+function mergeSorted(arr1, arr2) {
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+console.log(mergeSorted([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]MM
