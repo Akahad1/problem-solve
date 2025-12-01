625487671a37475ac2e5d6dd0d6043a59b050694
Akahad1 <aksahad234567@gmail.com>
2025-12-01 10:41:11 +0600

    add

--- problem4.js
+++ problem4.js
@@ -120,28 +120,3 @@ const books = [
   { title: "Moby Dick", author: "Herman Melville", year: 1851 },
   { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
 ];
-function throttle(func, delay) {
-  let lastCall = 0;
-  return (...args) => {
-    const now = Date.now();
-    if (now - lastCall >= delay) {
-      lastCall = now;
-      func(...args);
-    }
-  };
-}
-const persons = [
-  { name: "Rakib", age: 20, gender: "male" },
-  { name: "Ria", age: 20, gender: "Female" },
-  { name: "Nokive", age: 20, gender: "male" },
-  { name: "Shkive", age: 20, gender: "male" },
-  { name: "Piya", age: 20, gender: "Female" },
-];
-function longestWord(sentence) {
-  return sentence
-    .split(" ")
-    .reduce(
-      (longest, word) => (word.length > longest.length ? word : longest),
-      ""
-    );
-}
