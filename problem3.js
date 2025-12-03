61f8f9fa0fc826201b7ee06fd9e1720ee3fcefd1
Akahad1 <aksahad234567@gmail.com>
2025-12-03 17:37:48 +0600

    add

--- problem3.js
+++ problem3.js
@@ -59,3 +59,28 @@ const frequency = (arr) => {
 };
 
 console.log(frequency([1, 2, 2, 3, 3, 3])); // {1: 1, 2: 2, 3: 3}
+function throttle(func, delay) {
+  let lastCall = 0;
+  return (...args) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+const persons = [
+  { name: "Rakib", age: 20, gender: "male" },
+  { name: "Ria", age: 20, gender: "Female" },
+  { name: "Nokive", age: 20, gender: "male" },
+  { name: "Shkive", age: 20, gender: "male" },
+  { name: "Piya", age: 20, gender: "Female" },
+];
+function longestWord(sentence) {
+  return sentence
+    .split(" ")
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ""
+    );
+}
