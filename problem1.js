42ec81ff4e66724c52ec9f6e4427604ab0969ecd
Akahad1 <aksahad234567@gmail.com>
2025-12-03 17:37:11 +0600

    add

--- problem1.js
+++ problem1.js
@@ -59,3 +59,34 @@ const frequency = (arr) => {
 };
 
 console.log(frequency([1, 2, 2, 3, 3, 3])); // {1: 1, 2: 2, 3: 3}
+function modifyAgeByName(peopleArray, personName, newAge) {
+  const person = peopleArray.find((p) => p.name === personName);
+
+  if (person) {
+    person.age = newAge;
+  }
+
+  return peopleArray;
+}
+const updatedPeople = modifyAgeByName(people, "piya", 32);
+console.log(updatedPeople);
+const uniqueNumbers = (arr) => {
+  const freq = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freq)
+    .filter((key) => freq[key] === 1)
+    .map(Number);
+};
+
+console.log(uniqueNumbers([1, 2, 2, 3, 4, 4])); // [1, 3]
+function capitalizeWords(sentence) {
+  return sentence.replace(/\b\w/g, (char) => char.toUpperCase());
+}
+console.log(capitalizeWords("hello world"));
+
+console.log(longestWord("I love programming"));
+
+console.log(findIntersection([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
